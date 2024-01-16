75b4d3799cbe41b7114b954f24892f103335d9c2
ashish007-ux <89539996+ashish007-ux@users.noreply.github.com>
2024-01-16 13:10:01 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
+import path from 'path';
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -16,7 +18,8 @@ mongoose
     console.log(err);
   });
 
-
+  const _dirname=path.resolve();
+  
 
 
 const app = express();
@@ -33,6 +36,13 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(_dirname,'client/dist')));
+app.get('*',
+  (req,res)=>{
+    res.sendFile(path.join(_dirname,'client','dist','index.html'))
+  }
+)
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -42,3 +52,4 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
